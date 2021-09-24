8240b9e3dc153687b6dc48173287655d4cfb873b
Tom Ames <tsames1@gmail.com>
2021-09-24 15:52:01 -0700

    added show, new, and update routes.

--- server.js
+++ server.js
@@ -10,15 +10,30 @@ const budget = require("./models/budget");
  Middleware
 *********************/
 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'))
 
 /*********************
  Routes
 *********************/
 
-app.get('',(req,res) => {
+app.get('/budget',(req,res) => {
   res.render("index.ejs", {budget: budget});
-})
+});
+
+app.get('/budget/new', (req,res) => {
+  res.render("new.ejs");
+});
+
+app.post('/budget', (req,res) => {
+  budget.push(req.body);
+  console.log(`New budget, ${req.body.name}, created!`);
+  res.redirect('/budget');
+});
+
+app.get('/budget/:id', (req,res) => {
+  res.render("show.ejs", {budget: budget[req.params.id]})
+});
 
 /*********************
  Server Listener
