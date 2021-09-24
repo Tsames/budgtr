72854949ab4cd622113db4238016a9d094e7e0da
Tom Ames <tsames1@gmail.com>
2021-09-24 14:35:16 -0700

    Added html table to index.ejs

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static('public'))
 *********************/
 
 app.get('',(req,res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", {budget: budget});
 })
 
 /*********************
